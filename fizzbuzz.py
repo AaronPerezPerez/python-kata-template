e9277bbff6f49b5109b9c0ecd1daae62e636751f
Aaron Perez <aarperper@gmail.com>
2023-05-08 15:34:00 +0200

    chore: initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+
+
+def convert(number: int) -> str:
+    return "Error"
+
+
+if __name__ == '__main__':
+    print('Hello World!')
+
---
+++ test_fizzbuzz.py
@@ -0,0 +1,12 @@
+import unittest
+
+import fizzbuzz
+
+
+class FizzBuzzTest(unittest.TestCase):
+    def test_should_return_the_number_as_string(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
